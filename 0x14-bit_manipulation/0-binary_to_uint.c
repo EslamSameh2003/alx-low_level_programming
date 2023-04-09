cdb1f94f37c0a2cda4a13210131089afccfcd8af
EslamSameh2003 <eslamsameh0081@gmail.com>
2023-04-09 21:34:09 +0200

    add

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int x;
+    
     unsigned int dec_val = 0;
 
     if (!b)
         return (0);
 
-    for (x = 0; b[x]; x++)
+    for (int x = 0; b[x]; x++)
     {
-        if (b[x] < '0' || b[x] > '1')
+        if (b[i] < '0' || b[x] > '1')
             return (0);
         dec_val = 2 * dec_val + (b[x] - '0');
     }
