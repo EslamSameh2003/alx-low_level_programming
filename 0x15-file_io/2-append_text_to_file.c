50812408b8078fe37074c768516068ce0cf90371
EslamSameh2003 <eslamsameh0081@gmail.com>
2023-04-10 22:26:42 +0200

    add

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,7 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		{
 			w++;
 		}
-		app_status = write(f, text_content, w);
+		 status = write(f, text_content, w);
 
 		if (status == -1) 
 		{
